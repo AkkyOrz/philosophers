bdaac22475a8074dca94843eee1f47a9b6cead07
akito <akito@student.42tokyo.jp>
2022-02-22 12:59:14 +0900

    sleep の関数を変更

--- philo/sleep_in_ms.c
+++ philo/sleep_in_ms.c
@@ -6,15 +6,16 @@ void sleep_in_ms(int wait_ms)
 	size_t starts_at;
 	size_t ends_at;
 	size_t sleep_in;
+	size_t now;
 
 	starts_at = get_time_ms();
 	ends_at = starts_at + wait_ms;
-	while (get_time_ms() < ends_at)
+	now = get_time_ms();
+	while (now < ends_at)
 	{
-		sleep_in = (ends_at - get_time_ms()) / 2;
+		sleep_in = (ends_at - now) / 2;
 		if (sleep_in > 0)
 			usleep(sleep_in * 1000);
-		else
-			usleep(100);
+		now = get_time_ms();
 	}
 }
