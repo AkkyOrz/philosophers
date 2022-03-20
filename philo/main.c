ec46f2e6799bec11766f09bb3decf1a0c48ba9e7
akito <akito@student.42tokyo.jp>
2022-03-20 19:14:20 +0900

    remove all

--- philo/main.c
+++ philo/main.c
@@ -1,21 +1,7 @@
 #include "philo.h"
-#include <pthread.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <unistd.h>
 
-int	main(int argc, char **argv)
-{
-	t_var	**vars;
-
-	if (!init_var(argc, argv, &vars))
-	{
-		delete_vars(&vars);
-		return (EXIT_FAILURE);
-	}
-	if (!start_philosophers(vars))
-		return (EXIT_FAILURE);
-	delete_vars(&vars);
+int main(int argc, char **argv){
+	(void)argc;
+	(void)argv;
+	return 0;
 }
